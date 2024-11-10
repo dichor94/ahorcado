bce79dc2c583dadd44e049b6c78056f400e5d35c
dichor94 <dichor@alumnatflorida.es>
2024-11-10 17:57:30 +0100

    Hago modificaciones en el archivo index.php y pongo los primeros estilos en styles.css.
    En INDEX.PHP pongo el enlace al archivo de estilos STYLES.CSS para que coja los estilos codificados.
    
    En STYLES.CSS pongo el estilos para el body y el título principal con selector h1. Se ha decidido poner los elementos en el centro mediante un display flex en modo columna y center.
    
    El título principal lo hemos puesto de color verde

--- index.php
+++ index.php
@@ -54,6 +54,7 @@ if ($_SESSION['letras_acertadas'] == $_SESSION['palabra']) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="./styles/styles.css">
     <title>Ahorcado</title>
 </head>
 <body>
